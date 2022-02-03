f742a8e0991aeba2913202ca1d640bb2d53da826
Damans227 <61474540+Damans227@users.noreply.github.com>
2022-02-02 23:55:15 -0500

    use s3 for file upload and restrict file type to sound

--- frontend/src/components/uploadItem.js
+++ frontend/src/components/uploadItem.js
@@ -1,22 +1,61 @@
 import React, { useState } from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import { Input } from '@mui/material';
+import { Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import AWS from 'aws-sdk'
+
+const S3_BUCKET = 'speakitaudiobucket/files';
+const REGION = 'us-east-1';
+
+AWS.config.update({
+    accessKeyId: '',
+    secretAccessKey: ''
+})
+
+const myBucket = new AWS.S3({
+    params: { Bucket: S3_BUCKET },
+    region: REGION,
+})
+
+const Input = styled('input')({
+    color: 'darkslategray',
+    backgroundColor: 'aliceblue',
+    padding: 8,
+    borderRadius: 4,
+});
 
 function UploadItem(props) {
 
-    let [audioFile, setAudioFile] = useState(null);
+    const [progress, setProgress] = useState(0);
+    const [selectedFile, setSelectedFile] = useState(null);
 
-    function fileUploadHandler(event) {
-        console.log(audioFile);
-
+    const handleFileInput = (e) => {
+        setSelectedFile(e.target.files[0]);
     }
-    console.log(audioFile)
+    const uploadFile = (file) => {
+
+        const params = {
+            ACL: 'public-read',
+            Body: file,
+            Bucket: S3_BUCKET,
+            Key: file.name
+        };
+
+        myBucket.putObject(params)
+            .on('httpUploadProgress', (evt) => {
+                setProgress(Math.round((evt.loaded / evt.total) * 100))
+            })
+            .send((err) => {
+                if (err) console.log(err)
+            })
+    }
 
     return (
         <Stack spacing={2} direction="row">
-            <Input type="file" onChange={(e) => { setAudioFile(e.target.files[0]) }} />
-            <Button onClick={fileUploadHandler} variant="outlined" style={{ width: 1000 }}>UPLOAD AUDIO</Button>
+            <Typography> Upload Progress is {progress}%</Typography>
+            <Input accept="audio/*" type="file" onChange={handleFileInput} />
+            <Button onClick={() => uploadFile(selectedFile)} variant="outlined" style={{ width: 800 }}>UPLOAD AUDIO</Button>
         </Stack>
     );
 }
