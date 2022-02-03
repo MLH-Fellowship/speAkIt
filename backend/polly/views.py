c37322242d0cb4b1e87a78b4b1ef73ac8d496aa1
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2022-02-03 15:58:13 -0600

    simple endpoint integration

--- backend/polly/views.py
+++ backend/polly/views.py
@@ -7,16 +7,25 @@ from rest_framework.response import Response
 from rest_framework import status
 from . serializers import SnippetSerializer
 from rest_framework import serializers
+from . aws_polly import PollyService
+from rest_framework.renderers import JSONRenderer
 
 class PollyViewSet(APIView):
+    def __init__(self, bucket_name="speakit123125467") -> None:
+        self.bucket = bucket_name
+        self.polly_service = PollyService()
+        self.polly_service.create_bucket(self.bucket)
+
     def get(self, request):
         data = request.data             # Data passed in body
-        print(data)
+        # audio_url = self.polly_service.retrieve_audio_url(bucket=self.bucket, identifier="testing1234")
         return Response({"message": "All good get"})
 
     def post(self, request):
-        serializer = SnippetSerializer(data=request.data.get('user'))
-        if serializer.is_valid():
-            # serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        user_input = request.data["user_text"]
+        identifier = request.data["identifier"]
+        print(user_input, identifier)
+        # polly_post = self.polly_service.use_polly(user_input, self.bucket, identifier)
+            
+        return Response({"message": "working"})
