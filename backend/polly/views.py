a5c8d1aeabf96eef83aec3d9dfa63260be8f594f
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2022-02-03 17:31:57 -0600

    cleaned up the code, ready to use

--- backend/polly/views.py
+++ backend/polly/views.py
@@ -1,31 +1,37 @@
-from email import message
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from . serializers import SnippetSerializer
-from rest_framework import serializers
 from . aws_polly import PollyService
-from rest_framework.renderers import JSONRenderer
 
 class PollyViewSet(APIView):
+    # Endpoints class for the AWS Polly services
     def __init__(self, bucket_name="speakit123125467") -> None:
         self.bucket = bucket_name
         self.polly_service = PollyService()
         self.polly_service.create_bucket(self.bucket)
 
     def get(self, request):
-        data = request.data             # Data passed in body
+        # the get identifier is the same as the post identifier
+        audio_identifier = request.query_params.get('identifier')    
+        if not audio_identifier:
+            return Response({"message": "Please provide the identifier of the audio"})
         # audio_url = self.polly_service.retrieve_audio_url(bucket=self.bucket, identifier="testing1234")
-        return Response({"message": "All good get"})
+        audio_url = self.polly_service.retrieve_audio_url(bucket=self.bucket, identifier=audio_identifier)
+        return Response({"audio_url": audio_url})
 
     def post(self, request):
-
+        """JSON requiered for the POST:
+            {
+                "user_text": str,
+                "identifier": str 
+            }
+        """
         user_input = request.data["user_text"]
         identifier = request.data["identifier"]
-        print(user_input, identifier)
-        # polly_post = self.polly_service.use_polly(user_input, self.bucket, identifier)
-            
-        return Response({"message": "working"})
+
+        polly_response = self.polly_service.use_polly(user_input, self.bucket, identifier)            
+        
+        if polly_response:
+            return Response({"message": "Successfully started polly job", "aws_polly_response": polly_response})
+        else:
+            return Response({"message": "Failed to start polly job", "aws_polly_response": polly_response})
