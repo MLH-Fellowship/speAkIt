9f995e8e562c8681af4687e62ffa8a1e37fda571
Damans227 <61474540+Damans227@users.noreply.github.com>
2022-02-03 22:53:13 -0500

    all files untill s3 upload

--- frontend/src/components/input.js
+++ frontend/src/components/input.js
@@ -2,17 +2,18 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import UploadItem from './uploadItem';
 
-function TextInput(props) {
+function UserInput(props) {
     return (
         <form>
             <TextField multiline rows={10} id="outlined-basic" label="Speech Text" variant="filled" style={{ marginTop: 5, marginBottom: 22, width: 1000 }} focused />
             <Stack spacing={2} direction="column">
-                <Button variant="outlined">Record</Button>
-                <Button variant="outlined">Upload</Button>
+                <Button variant="contained">SUBMIT TEXT</Button>
+                <UploadItem />
             </Stack>
         </form>
     );
 }
 
-export default TextInput;
+export default UserInput;
