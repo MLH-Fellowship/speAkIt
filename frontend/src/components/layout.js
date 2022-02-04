faecc9a1c442603296d5709f128e64f880320fca
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2022-02-04 08:22:18 -0600

    remove margin left ml

--- frontend/src/components/layout.js
+++ frontend/src/components/layout.js
@@ -9,17 +9,23 @@ import Output from './output';
 
 
 function Layout(props) {
+    const myComponentStyle = {
+        padding: '1.5em',
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        }
     return (
-        <div style={{ marginTop: `20px`, marginLeft: `122px` }}>
+        <div style={ myComponentStyle }>
             <Box sx={{ width: 1 }}>
                 <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
-                    <Box sx={{ ml: 40, width: 1000 }} gridColumn="span 10">
+                    <Box sx={{ width: 1000 }} gridColumn="span 10">
                         <Instructions />
                     </Box>
-                    <Box sx={{ ml: 40, width: 1000 }} gridColumn="span 10">
+                    <Box sx={{width: 1000 }} gridColumn="span 10">
                         <UserInput />
                     </Box>
-                    <Box sx={{ ml: 40, width: 1000 }} gridColumn="span 10">
+                    <Box sx={{width: 1000 }} gridColumn="span 10">
                         <Output />
                     </Box>
                 </Box>
