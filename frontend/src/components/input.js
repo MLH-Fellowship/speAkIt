9863954fb83ac5058cda148966f86030e13fe7a8
Damans227 <61474540+Damans227@users.noreply.github.com>
2022-02-04 12:19:28 -0500

    Added media player

--- frontend/src/components/input.js
+++ frontend/src/components/input.js
@@ -4,12 +4,14 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import UploadItem from './uploadItem';
+import ReactAudioPlayer from 'react-audio-player';
 
 function UserInput(props) {
 
     const POLLY_URL = `${process.env.REACT_APP_APIURL}/polly`;
 
     const [text, setText] = useState("");
+    const [audio, setAudio] = useState(null);
 
     function textHandler(e) {
         setText(e.target.value)
@@ -19,13 +21,15 @@ function UserInput(props) {
     function submitHandler() {
         let polly_request = {
             user_text: text,
-            identifier: "polly/"+Date.now()
+            identifier: "polly/" + Date.now()
         }
         console.log(polly_request);
         axios.post(POLLY_URL, polly_request).then((res => {
             console.log(res.data.aws_polly_response.SynthesisTask.OutputUri);
+            setAudio(res.data.aws_polly_response.SynthesisTask.OutputUri)
             axios.get(res.data.aws_polly_response.SynthesisTask.OutputUri).then((res) => {
                 console.log(res);
+
             })
 
         })
@@ -37,7 +41,15 @@ function UserInput(props) {
             <TextField onChange={textHandler} inputProps={{ maxLength: 250 }} multiline rows={10} id="outlined-basic" label="Speech Text" variant="filled" style={{ marginTop: 5, marginBottom: 22, width: 1000 }} focused />
             <Stack spacing={2} direction="column">
                 <Button onClick={submitHandler} variant="contained">SUBMIT TEXT</Button>
+                <ReactAudioPlayer
+                    src={audio}
+                    autoPlay
+                    controls
+                    style={{ width: '1000px' }}
+                />
+
                 <UploadItem />
+
             </Stack>
         </form>
     );
