5a9ac798ab8bb58f4656716c4b034011f5812615
Damans227 <61474540+Damans227@users.noreply.github.com>
2022-02-04 04:46:49 -0500

    succesfully fetch data

--- frontend/src/components/uploadItem.js
+++ frontend/src/components/uploadItem.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 import axios from 'axios';
+
+
 
 const Input = styled('input')({
     color: 'darkslategray',
@@ -17,41 +19,48 @@ function UploadItem(props) {
     const TRANSCRIBE_URL = `${process.env.REACT_APP_UPLOAD}:8000/transcribe`;
 
     let [audioFile, setAudioFile] = useState(null);
-    let [sThreeUrl, setSThreeUrl] = useState(null);
-    // let [transcriptionJob, setTranscriptionJob] = useState(null);
+    let [transcriptionJob, setTranscriptionJob] = useState("");
+
+    useEffect(() => {
+        console.log(transcriptionJob);
+        setTimeout(() => axios.get(`${TRANSCRIBE_URL}?transcription-job-name=${transcriptionJob}`).then(res => { console.log(res) }), 30000)
+    }, [TRANSCRIBE_URL, transcriptionJob])
 
     function fileUploadHandler(event) {
-        event.preventDefault()
         let data = new FormData()
+        event.preventDefault()
+
         data.append("audio_file", audioFile);
         axios.put(UPLOAD_URL, data, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
         }).then(res => {
-            setSThreeUrl(res.data.s3_url)
-
-        }).catch((error) => {
-            console.log(error);
-        });
+            console.log(res.data.s3_url);
+            return res.data.s3_url
+        }).then(s3_url => {
+            let objectFromSThree = {
+                s3_url: s3_url,
+                language_code: "en-US"
+            }
+            console.log(objectFromSThree)
+            return objectFromSThree
+        }).then(objectFromSThree => { return axios.post(TRANSCRIBE_URL, objectFromSThree) })
+            .then(res => { return res.data.transcription_job_name })
+            .then((transcriptionJobName) => {
+                console.log(transcriptionJobName)
+                return transcriptionJobName
+            }).then(transcriptionJobName => {
+                setTranscriptionJob(transcriptionJobName)
+                console.log(transcriptionJob);
+                return axios.get(`${TRANSCRIBE_URL}?transcription-job-name=${transcriptionJobName}`)
+            }).then(res => {
+                console.log(res)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
-    let objectFromSThree = {
-        s3_url: sThreeUrl,
-        language_code: "en-US"
-    }
-
-    if (objectFromSThree.s3_url != null) {
-        console.log(objectFromSThree);
-        axios.post(TRANSCRIBE_URL, objectFromSThree).then(res => {
-
-
-
-        }).catch((error) => {
-            console.log(error);
-        });
-
-    }
-
 
     return (
         <Stack spacing={2} direction="row">
