4fbf186fe576ac88d634221e9de1cbd8d11e8350
Hugo <hugointe@gmail.com>
2022-02-14 11:25:02 -0300

    cors allow all origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,8 +61,8 @@ MIDDLEWARE = [
 ]
 
 # # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
-# CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000', 'http://localhost:3000', 'http://127.0.0.1:8000', 'http://127.0.0.1:3000', env(
         'BACKEND_URL'), env('FRONTEND_URL')
